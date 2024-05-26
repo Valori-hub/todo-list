187f8665deca8a06504aed8c2365c5402b706294
Adam_Elcner <mradam621@gmail.com>
2024-05-26 07:21:21 +0200

    transfer data beetwen devices

--- todo-list/src/app/app.routes.ts
+++ todo-list/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { SignInComponent } from './components/sign-in-conmponent/sign-in-conmponent.component'
+import { SignInComponent } from './components/sign-in-component/sign-in-conmponent.component';
 import { AuthenticatorComponent } from './pages/authenticator/authenticator.component';
 
 export const routes: Routes = [
@@ -8,5 +8,4 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/authenticator', pathMatch: 'full' },
   { path: 'login', component: SignInComponent },
-  
 ];
--- todo-list/src/app/pages/authenticator/authenticator.component.ts
+++ todo-list/src/app/pages/authenticator/authenticator.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpService } from '../../http-service.service';
-import { SignUpComponent } from "../../components/sign-up-conmponent/sign-up-conmponent.component";
-import { SignInComponent } from "../../components/sign-in-conmponent/sign-in-conmponent.component";
+import { SignUpComponent } from '../../components/sign-up-component/sign-up-conmponent.component';
+import { SignInComponent } from '../../components/sign-in-component/sign-in-conmponent.component';
 
 @Component({
-    selector: 'app-authenticator',
-    standalone: true,
-    templateUrl: './authenticator.component.html',
-    styleUrl: './authenticator.component.scss',
-    imports: [RouterOutlet, SignUpComponent, SignInComponent]
+  selector: 'app-authenticator',
+  standalone: true,
+  templateUrl: './authenticator.component.html',
+  styleUrl: './authenticator.component.scss',
+  imports: [RouterOutlet, SignUpComponent, SignInComponent],
 })
-export class AuthenticatorComponent {
-
-
-}
+export class AuthenticatorComponent {}
